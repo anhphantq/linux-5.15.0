80a826b4cc083f305658aa7bc1722fe47e4c9593
root <root@>
2023-07-09 22:46:24 -0400

    update file

---
+++ compile-kernel.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm -y
+sudo apt install git build-essential fakeroot libncurses5-dev libssl-dev ccache -y 
+apt install libslang2-dev liblzma-dev libnuma-dev libbabeltrace-ctf-dev default-jdk libdw-dev dwarves libcap-dev libzstd-dev systemtap-sdt-dev libunwind-dev -y
